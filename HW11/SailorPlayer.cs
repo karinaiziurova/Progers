329ce29c1434e27c41ed0a450af6c5a0facb0f39
karinaiziurova <karishaiziurova@gmail.com>
2021-11-17 09:32:52 +0200

    adding project
    Signed-off-by: karinaiziurova <karishaiziurova@gmail.com>

---
+++ HW11/SailorPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;  //доступ к объектам памяти при условии что тип объекта реализует интерфейс   
+using System.Text; //вмещает классы, дополнительные базы классов и вспомогательные классы
+
+namespace HW11
+{
+    class SailorPlayer
+    {
+        public string Name { get; set; } //вседоступный параметр
+
+        public int[,] SeaField { get; set; } //определяет неизменяемую структуру (свойства доступны только для чтения)
+
+        public SailorPlayer(string name)
+        {
+            Name = name;
+        }
+    }
+}
